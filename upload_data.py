7fd2478af287e0a414863c928e53035c2186808a
Bill Yang <joyang12345@gmail.com>
2025-02-10 17:39:52 -0500

    Updated Data Upload File

---
+++ upload_data.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import json
+
+from elasticsearch import Elasticsearch
+
+print("Starting...\n")
+
+client = Elasticsearch(
+  "https://localhost:9200",
+  api_key=""
+)
+
+# Load TV
+# df = pd.read_csv("data/TMDB_tv_dataset_v3.csv")
+
+# Movie
+# df = pd.read_csv("data/TMDB_movie_dataset_v11.csv")
+
+# Game
+df = pd.read_csv("data/backlogged_games.csv")
+
+for start in range(0, len(df), 5000):
+    chunk = df.iloc[start:start + 5000]
+
+    # Convert to Elasticsearch bulk JSON format
+    bulk_data = ""
+    for i, row in chunk.iterrows():
+        bulk_data += json.dumps({"index": {"_index": "game", "_id": str(i)}}) + "\n"
+        bulk_data += row.to_json() + "\n"
+    
+    client.bulk(operations=bulk_data, pipeline="ent-search-generic-ingestion")
+
+print("Finish\n")
