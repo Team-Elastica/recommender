f6737479d0c3aa8c4163beb4f83b572cc311ed82
Bill Yang <joyang12345@gmail.com>
2025-03-18 22:16:49 -0400

    Template Semantic search

---
+++ src/semanticSearch.py
@@ -0,0 +1,30 @@
+from sentence_transformers import SentenceTransformer
+from elasticsearch import Elasticsearch
+import os
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+client = Elasticsearch(
+  "https://localhost:9200",
+  api_key=os.getenv("API")
+)
+
+inputQuery = "Death Stranding 2"
+
+# https://medium.com/@bairagiabhishek03/elasticsearch-as-a-vector-store-es-tutorial-5-816f9451ddc1
+query = {
+    "script_score": {
+        "query": {"match_all": {}},
+        "script": {
+            "source": "cosineSimilarity(params.query_vector, 'embedding') + 1.0",
+            "params": {
+                "query_vector": model.encode(inputQuery).tolist()
+            }
+        }
+    }
+}
+
+response = client.search(size=5, source_excludes='embedding' ,index='semantic_game', query=query)
+
+for hit in response['hits']['hits']:
+    print(hit['_source']['Title'])
